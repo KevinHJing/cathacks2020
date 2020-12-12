e455a1bd1ccc9fee3514836dd71684dfc39ff88c
ssbhatt4321 <58531376+ssbhatt4321@users.noreply.github.com>
2020-12-12 15:12:24 -0500

    basic image click response

--- src/controllers/GameScreenController.java
+++ src/controllers/GameScreenController.java
@@ -1,31 +1,22 @@
 package controllers;
 import javafx.animation.FadeTransition;
-import game.*;
 
 import game.Character;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.util.Duration;
 
+import javax.swing.text.html.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import java.awt.*;
-import java.io.IOException;
-
-public class GameScreenController implements Initializable{
+public class GameScreenController implements Initializable {
 
     @FXML
     Label dialogue;
@@ -51,16 +42,21 @@ public class GameScreenController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         GameRoot.setOpacity(0);
-        makeFadeInTrasition();
+        makeFadeInTransition();
     }
 
-    private void makeFadeInTrasition() {
+    private void makeFadeInTransition() {
         FadeTransition fadeout = new FadeTransition();
         fadeout.setDuration(Duration.millis(1000));
         fadeout.setNode(GameRoot);
         fadeout.setFromValue(0);
         fadeout.setToValue(1);
         fadeout.play();
+    }
+
+    @FXML
+    public void imageClick(javafx.scene.input.MouseEvent mouseEvent) {
+        dialogue.setText("You clicked the image!!!!!");
     }
 }
 
