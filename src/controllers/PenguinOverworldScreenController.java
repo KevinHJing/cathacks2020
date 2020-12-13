497907ef4e8ca80cf97ba7fe8e968cea5cd30c51
Ila Sharma <isharma37@gatech.edu>
2020-12-12 19:30:17 -0800

    penguin and reindeer disappear after visited

--- src/controllers/PenguinOverworldScreenController.java
+++ src/controllers/PenguinOverworldScreenController.java
@@ -4,9 +4,11 @@ import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -19,11 +21,20 @@ import java.util.ResourceBundle;
 
 public class PenguinOverworldScreenController implements Initializable {
 
+    static boolean penguinVisited = false;
     @FXML AnchorPane GameRoot;
+    @FXML ImageView imageView;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         GameRoot.setOpacity(0);
         makeFadeInTransition();
+
+        if (penguinVisited) {
+            imageView.setImage(null);
+            imageView.setMouseTransparent(false);
+            imageView.setDisable(true);
+        }
     }
 
     private void makeFadeInTransition() {
@@ -36,6 +47,7 @@ public class PenguinOverworldScreenController implements Initializable {
     }
 
     public void loadNextScene(MouseEvent mouseEvent) throws IOException {
+        penguinVisited = true;
         Parent secondView;
         secondView = (StackPane) FXMLLoader.load(getClass().getResource("fxml/PenguinDialogScreen.fxml"));
         Scene scene = new Scene(secondView, 900, 650);
